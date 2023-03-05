59277de386ac253f2a4c65bdc1af877159f5d480
dogukanakkaya <doguakkaya27@hotmail.com>
2023-03-05 15:42:00 +0300

    feat: create the role for lambda and add queue for sending mails

--- todo/insert.ts
+++ todo/insert.ts
@@ -1,31 +1,46 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
 import { nanoid } from 'nanoid';
 import { z } from 'zod';
 
-const dynamoDBClient = new DynamoDBClient({ region: "us-east-1" });
+const config = {
+    REGION: 'us-east-1',
+    TODO_TABLE_NAME: process.env.TODO_TABLE_NAME as string,
+    EMAIL_QUEUE_URL: process.env.EMAIL_QUEUE_URL as string
+} as const;
+
+const dynamoDBClient = new DynamoDBClient({ region: config.REGION });
+const sqsClient = new SQSClient({ region: config.REGION });
 
 const Todo = z.object({
     title: z.string().max(25),
-    description: z.string()
+    description: z.string(),
+    assignee: z.string().email()
 });
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
         if (!event.body) throw Error();
+
         const todo = await Todo.parseAsync(JSON.parse(event.body));
         const id = nanoid();
 
-        const params = {
-            TableName: 'TodoTable',
+        await dynamoDBClient.send(new PutItemCommand({
+            TableName: config.TODO_TABLE_NAME,
             Item: {
                 id: { S: id },
                 title: { S: todo.title },
-                description: { S: todo.description }
+                description: { S: todo.description },
+                assignee: { S: todo.description }
             }
-        };
+        }));
 
-        await dynamoDBClient.send(new PutItemCommand(params));
+        const x = await sqsClient.send(new SendMessageCommand({
+            QueueUrl: config.EMAIL_QUEUE_URL,
+            MessageBody: JSON.stringify({ todo }),
+        }));
+        console.log("message sent", x.MessageId);
 
         return {
             statusCode: 200,
